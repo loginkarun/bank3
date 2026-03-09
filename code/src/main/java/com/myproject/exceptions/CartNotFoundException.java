8e0974e10fb993996b246578343eb8253ff19fa3
loginkarun <loginkarun@gmail.com>
2026-03-09 19:21:45 +0530

    Auto commit: Scripts committed on 2026-03-09-13-51-43

--- code/src/main/java/com/myproject/exceptions/CartNotFoundException.java
+++ code/src/main/java/com/myproject/exceptions/CartNotFoundException.java
@@ -1,9 +1,7 @@
 package com.myproject.exceptions;
 
-/**
- * Exception thrown when cart is not found
- */
 public class CartNotFoundException extends RuntimeException {
+    
     public CartNotFoundException(String message) {
         super(message);
     }
