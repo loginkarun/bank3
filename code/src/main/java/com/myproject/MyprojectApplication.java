04eecfc2c020d760c72da976a44f88e014b8a9c9
loginkarun <loginkarun@gmail.com>
2026-03-09 19:24:50 +0530

    Auto commit: Scripts committed on 2026-03-09-13-54-49

--- code/src/main/java/com/myproject/MyprojectApplication.java
+++ code/src/main/java/com/myproject/MyprojectApplication.java
@@ -2,14 +2,12 @@ package com.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/**
- * Main Application class for myproject Shopping Cart Application
- * Configured for Spring Boot 3.5.9 with JDK 21
- */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.myproject.models.repositories")
 public class MyprojectApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(MyprojectApplication.class, args);
     }
